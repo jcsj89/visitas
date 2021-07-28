8a869e10e519865b9a93e711da1ec703c5f38cb6
Jose Carlos <jcsj2010@gmail.com>
2021-07-28 18:47:15 -0300

    28.07

--- src/modules/visitor/controllers/RolVisitasController.ts
+++ src/modules/visitor/controllers/RolVisitasController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import knex from '../../../database/connection';
-import { v4 } from 'uuid';
+import { stringify, v4 } from 'uuid';
+import { copyFile } from 'fs';
 
 export default class RolVisitasController {
     public async index(
@@ -33,4 +34,98 @@ export default class RolVisitasController {
 
         return response.json(visitors);
     }
+
+    public async search(
+        request: Request,
+        response: Response,
+    ): Promise<Response | void> {
+        const { cpf } = request.body;
+
+        //se a entrada for null
+        if (!cpf) {
+            //seta a flash message
+            await request.flash('info', 'Entre com um CPF.');
+            //consome a flash message, como nao vai redirecionar
+            const messages = await request.consumeFlash('info');
+
+            return response.render('index', { messages });
+        }
+
+        //se não tiver 11 caracteres
+        if (cpf.length !== 11) {
+            //seta a flash message
+            await request.flash(
+                'info',
+                'Formato do CPF Inválido. Digite somente dígitos.',
+            );
+            //consome a flash message, como nao vai redirecionar
+            const messages = await request.consumeFlash('info');
+
+            return response.render('index', { messages });
+        }
+
+        if (!testaCPF(cpf)) {
+            //seta a flash message
+            await request.flash('info', 'CPF Inválido.');
+            //consome a flash message, como nao vai redirecionar
+            const messages = await request.consumeFlash('info');
+
+            return response.render('index', { messages });
+        }
+
+        const visitor = await knex('visitors')
+            .where({
+                VIS_CPF: cpf,
+            })
+            .select(
+                'VIS_NOME',
+                'DET_NOME',
+                'DET_MATRICULA',
+                'INC_RAIO',
+                'INC_CELA',
+                'VIS_BLOQUEADO',
+                'VIS_OBS',
+            );
+
+        if (visitor.length < 1) {
+            return response.json({ msg: 'CPF nao encontrado.' });
+        }
+
+        //visitor[0]['VIS_OBS'] = '';
+        console.log(visitor);
+        return response.render('index', { visitor });
+    }
+}
+
+//function for testing cpf validation
+function testaCPF(strCPF: string): boolean {
+    let Soma;
+    let Resto;
+    Soma = 0;
+    if (strCPF === '00000000000') return false;
+    if (strCPF === '11111111111') return false;
+    if (strCPF === '22222222222') return false;
+    if (strCPF === '33333333333') return false;
+    if (strCPF === '44444444444') return false;
+    if (strCPF === '55555555555') return false;
+    if (strCPF === '66666666666') return false;
+    if (strCPF === '77777777777') return false;
+    if (strCPF === '88888888888') return false;
+    if (strCPF === '99999999999') return false;
+
+    for (let i = 1; i <= 9; i++)
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (let i = 1; i <= 10; i++)
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
 }
--- src/modules/visitor/routes/visitors.router.ts
+++ src/modules/visitor/routes/visitors.router.ts
@@ -5,6 +5,7 @@ import RolVisitasController from '@modules/visitor/controllers/RolVisitasControl
 const rolVisitasController = new RolVisitasController();
 
 rolStatusRoutes.get('/', rolVisitasController.index);
+rolStatusRoutes.post('/visitas/status', rolVisitasController.search);
 rolStatusRoutes.get('/visitas/list', rolVisitasController.list);
 
 export default rolStatusRoutes;
