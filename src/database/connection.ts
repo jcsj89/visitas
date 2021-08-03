bd96456ee2a0ce58008e1e961f1cb2cfa45dab63
Jose Carlos <jcsj2010@gmail.com>
2021-08-03 19:26:13 -0300

    ssl

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,10 @@
 import knex from 'knex';
 import { resolve } from 'path';
+import pg from 'pg';
 import dotenv from 'dotenv';
+
+pg.defaults.ssl = process.env.NODE_ENV === 'development' ? false : true;
+console.log(pg.defaults.ssl);
 dotenv.config({
     path: resolve(__dirname, '..', '..', '.env'),
 });
@@ -8,7 +12,11 @@ dotenv.config({
 const connection = knex({
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    //searchPath: ['knex', 'public'],
+    searchPath: ['knex', 'public'],
+    pool: {
+        min: 0,
+        max: 20,
+    },
     useNullAsDefault: true,
 });
 
